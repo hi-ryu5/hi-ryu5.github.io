3c47fb040a4fa56bffb435ad35014b251e9dba48
hi-ryu5 <63032332+hi-ryu5@users.noreply.github.com>
2020-09-21 17:18:40 +0900

    tet_fix

--- js/tetris.js
+++ js/tetris.js
@@ -2,16 +2,25 @@
 
 // 初期設定
 
-const canvas = document.querySelectorAll('canvas')
-const ctxui = canvas[0].getContext('2d')
-const ctxma = canvas[1].getContext('2d')
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
 
 const imgbl =new Image()
+const imgky =new Image()
 imgbl.src = 'pic/tetblock.gif'
-//const BGM = new Audio('sound/40-1.mp3')
+imgky.src = 'pic/key.gif'
+const BGM = new Audio('sound/katyusha.mp3')
+BGM.volume = 0.1
+BGM.loop = true
+
+let field,mblock,timer,keytime,score,lines,nblock,ncase,level,hold,f_end,f_key=false
 
 
-let field,mblock,timer
+function square(x,y,a,b,c,d){
+  return a<x && x<a+c && b<y && y<b+d
+}
+
+
 
 
 // キー入力処理
@@ -30,20 +39,106 @@ const keyInputSetting = () => {
     keypushing[e.keyCode] = false
   }
   ,false)
+  
+  
+  
+  
+  canvas.addEventListener( "touchstart",
+  function (e) {
+    let touchObject = e.changedTouches[0]
+    let clientRect = this.getBoundingClientRect()
+    let x = touchObject.pageX - (clientRect.left + window.pageXOffset)
+    let y = touchObject.pageY - (clientRect.top + window.pageYOffset)
+    console.log(x,y)
+    
+    if (square(x,y,10,390,80,80))
+      keydown[37] = true
+    if (square(x,y,90,310,80,80))
+      keydown[38] = true
+    if (square(x,y,90,390,80,80))
+      keydown[40] = true
+    if (square(x,y,170,390,80,80))
+      keydown[39] = true
+    if (square(x,y,310,390,80,80))
+      keydown[90] = true
+    if (square(x,y,390,390,80,80))
+      keydown[88] = true
+    if (square(x,y,390,310,80,80))
+      keydown[16] = true
+    if (square(x,y,390,10,80,80))
+      keydown[82] = true
+  }
+  ,false)
+  
+  canvas.addEventListener( "touchend",
+  function (e) {
+    let touchObject = e.changedTouches[0]
+    let clientRect = this.getBoundingClientRect()
+    let x = touchObject.pageX - (clientRect.left + window.pageXOffset)
+    let y = touchObject.pageY - (clientRect.top + window.pageYOffset)
+    console.log("end",x,y)
+    
+    if (square(x,y,10,390,80,80)){
+      keydown[37] = false
+      keypushing[37] = false
+    }
+    if (square(x,y,90,310,80,80)){
+      keydown[38] = false
+      keypushing[38] = false
+    }
+    if (square(x,y,90,390,80,80)){
+      keydown[40] = false
+      keypushing[40] = false
+    }
+    if (square(x,y,170,390,80,80)){
+      keydown[39] = false
+      keypushing[39] = false
+    }
+    if (square(x,y,310,390,80,80)){
+      keydown[90] = false
+      keypushing[90] = false
+    }
+    if (square(x,y,390,390,80,80)){
+      keydown[88] = false
+      keypushing[88] = false
+    }
+    if (square(x,y,390,310,80,80)){
+      keydown[16] = false
+      keypushing[16] = false
+    }
+    if (square(x,y,390,10,80,80)){
+      keydown[82] = false
+      keypushing[82] = false
+    }
+    
+  }
+  ,false)
+  
 }
 
 
-class MovingBlock {
-  constructor (x,y,type,rot) {
-    this.x = x
-    this.y = y
-    this.type = type
-    this.rot = rot
-  }
-  
-  bldict () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const BlockFu = {
+  bldict(type,rot){
     let data,center
-    switch (this.type){
+    
+    type = (type-1)%7+1
+    
+    switch (type){
       case 1:
       data = [[0,1],[1,0],[1,1],[2,1]]
       center = [1,1]
@@ -72,23 +167,49 @@ class MovingBlock {
       data = [[0,1],[1,1],[2,1],[3,1]]
       center = [1.5,1.5]
       break
+      default:
+      console.log("type未指定")
+      return
     }
     
     
     for (let i=0;i<4;i++){
       data[i] = [data[i][0]-center[0],data[i][1]-center[1]]
-      for (let j=0;j<this.rot;j++)
+      for (let j=0;j<rot;j++)
         data[i] = [-data[i][1],data[i][0]]
       data[i] = [Math.floor(data[i][0]+center[0]),Math.floor(data[i][1]+center[1])]
     }
     
     return data
+  },
+  
+  
+  bldisp(x,y,type,rot){
+    let data = this.bldict(type,rot)
+    for (let i=0;i<4;i++){
+      ctx.drawImage(imgbl,type%5*20,Math.floor(type/5)*20,20,20,140+(data[i][0]+x)*20,40+(data[i][1]+y)*20,20,20,)
+    }
   }
   
+  
+  
+}
+
+
+
+
+
+class MovingBlock {
+  constructor (x,y,type,rot) {
+    this.x = x
+    this.y = y
+    this.type = type
+    this.rot = rot
+  }
+  
+  
   hittest (vx,vy,vrot) {
-    this.rot += vrot
-    let data = this.bldict()
-    this.rot -= vrot
+    let data = BlockFu.bldict(this.type,this.rot+vrot)
     
     for (let i=0;i<4;i++){
       let nx = data[i][0]+this.x+vx
@@ -99,16 +220,16 @@ class MovingBlock {
           return true
         }
       }
+      
+      if (nx<1||10<nx)
+        return true
+      
     }
     return false
   }
   
   disp () {
-    ctxma.clearRect(0,0,480,480)
-    let data = this.bldict()
-    for (let i=0;i<4;i++){
-      ctxma.drawImage(imgbl,this.type%5*20,Math.floor(this.type/5)*20,20,20,140+(data[i][0]+this.x-1)*20,40+(data[i][1]+this.y)*20,20,20,)
-    }
+    BlockFu.bldisp(this.x-1,this.y-2,this.type,this.rot)
   }
   
 }
@@ -139,68 +260,200 @@ class Gamefield {
     [1,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1],
     ]
   }
   
   disp() {
-    ctxui.fillRect(0,0,480,480)
     let bl
     for (let i=0;i<20;i++){
       for (let j=0;j<10;j++){
-        bl = this.field[i][j+1]
-        ctxui.drawImage(imgbl,bl%5*20,Math.floor(bl/5)*20,20,20,140+j*20,40+i*20,20,20,)
+        bl = this.field[i+2][j+1]
+        ctx.drawImage(imgbl,bl%5*20,Math.floor(bl/5)*20,20,20,140+j*20,40+i*20,20,20,)
       }
     }
   }
   
   del () {
+    let deln=0
+    let f_bgm = false
+    
     for (let i=0;i<20;i++){
       let bl=0
       for (let j=0;j<10;j++){
-        if (this.field[i][j+1])
+        if (this.field[i+2][j+1])
           bl++
       }
       if (bl == 10){
         this.field.splice(i,1)
         this.field.splice(0,0,[1,0,0,0,0,0,0,0,0,0,0,1])
+        deln++
       }
       
-      
+      if (bl && !f_bgm){
+        if (i < 4){
+          BGM.playbackRate = 2.0
+        }else if (i < 8){
+          BGM.playbackRate = 1.5
+        }else{
+          BGM.playbackRate = 1.0
+        }
+        f_bgm = true
+      }
     }
+    
+    lines += deln
+    level = Math.floor(lines/10)+1
   }
   
 }
 
 
 
-
+const draw = () => {
+  ctx.fillStyle = '#000'
+  ctx.fillRect(0,0,480,480)
+  field.disp()
+  mblock.disp()
+  
+  for(let i=0;i<6;i++)
+    BlockFu.bldisp(12,i*3,nblock[i],0)
+  
+  if (hold[0])
+    BlockFu.bldisp(-5,0,hold[0],0)
+  
+  
+  //ui表示
+  ctx.fillStyle = '#FFF'
+  ctx.font = "16px 'sans-serif'"
+  ctx.fillText(`lines:${lines}`, 20, 280)
+  ctx.fillText(`level:${level}`, 20, 300)
+  ctx.fillText(`time:${(timer/60).toFixed(1)}`, 20, 340)
+  
+  let size = 60
+  let x = 20
+  let y = 400
+  let space = 80
+  
+  if (!f_key){
+  
+    ctx.shadowColor = "#FFF"
+    ctx.shadowBlur  = 0
+    ctx.shadowOffsetX = 1
+    ctx.shadowOffsetY = 1
+    
+    ctx.drawImage(imgky,0,0,40,40,x,y,size,size)
+    ctx.drawImage(imgky,40,0,40,40,x+space,y-space,size,size)
+    ctx.drawImage(imgky,80,0,40,40,x+space*2,y,size,size)
+    ctx.drawImage(imgky,120,0,40,40,x+space,y,size,size)
+    ctx.drawImage(imgky,0,40,40,40,320,400,size,size)
+    ctx.drawImage(imgky,40,40,40,40,400,400,size,size)
+    ctx.drawImage(imgky,80,40,40,40,400,320,size,size)
+    ctx.drawImage(imgky,120,40,40,40,400,20,size,size)
+    
+    
+    ctx.shadowColor = "#000"
+    ctx.shadowBlur  = 0
+    ctx.shadowOffsetX = 0
+    ctx.shadowOffsetY = 0
+  }
+}
 
 
 
 
 
 const main = () => {
-  let vx=0,vy=0,vrot=0,fre=20
   
-  if (!mblock)
-    mblock = new MovingBlock(4,-1,Math.floor( Math.random() * 7 ) + 1,0)
+  if (!f_end){
   
-  field.disp()
-  mblock.disp()
+  let vx=0,vy=0,vrot=0,vhard=0,gf=Math.ceil(20/level)*2
   
-  if(keydown[39]&&!keypushing[39]){
-    vx = 1
-    keypushing[39] = true
-  }else if(keydown[37]&&!keypushing[37]){
-    vx = -1
-    keypushing[37] = true
+  //next
+  
+  for (let i=0;i<8;i++){
+    if (ncase == 0)
+      ncase = [1,2,3,4,5,6,7]
+    
+    if (!nblock[i]){
+      let a = Math.floor(Math.random()*ncase.length)
+      nblock[i] = ncase[a]
+      ncase.splice(a,1)
+    }
+  }
+  
+  
+  //ブロック生成
+  
+  if (!mblock){
+    mblock = new MovingBlock(4,1,nblock[0],0)
+    nblock.splice(0,1)
+    hold[1] -= 1
+    
+    if(mblock.hittest(vx,0,0)){
+      f_end = true
+      BGM.pause()
+    }
+  }
+  
+  
+  
+  draw()
+  
+  
+  //ハードドロップ
+  {
+    while(!mblock.hittest(0,vhard,0))
+      vhard ++
+    
+    vhard -= 1
+    
+    BlockFu.bldisp(mblock.x-1,mblock.y+vhard-2,mblock.type+7,mblock.rot)
+    
+  }
+  
+  
+  
+  
+  // キー入力
+  
+  if(keydown[16]){
+    if (hold[1]<=0){
+      let a = hold
+      hold = [mblock.type,1]
+      
+      if (a[0] == 0){
+        mblock = new MovingBlock(4,-1,nblock[0],0)
+        nblock.splice(0,1)
+      }else{
+        mblock = new MovingBlock(4,-1,a[0],0)
+      }
+    }
+  }
+  
+  
+  if(keydown[39]){
+    if (keytime == 0 || keytime > 10)
+      vx = 1
+    keytime += 1
+  }else if(keydown[37]){
+    if (keytime == 0 || keytime > 10)
+      vx = -1
+    keytime += 1
   }else{
     vx=0
+    keytime = 0
+  }
+  
+  if(keydown[38]&&!keypushing[38]){
+    vy = vhard
+    keypushing[38] = true
   }
   
   if(keydown[40]){
-    fre = 2
+    gf = Math.min(Math.ceil(gf/2),2)
   }
   
   if(keydown[90]&&!keypushing[90]){
@@ -214,7 +467,12 @@ const main = () => {
   }
   
   
-  if (timer%fre == 0){
+  
+  
+  
+  
+  
+  if (timer%gf == 0){
     vy = 1
   }
   
@@ -222,16 +480,180 @@ const main = () => {
     vx = 0
   }
   
-  if(mblock.hittest(0,0,vrot)){
+  if(mblock.hittest(vx,vy,0)){
+    vx = 0
+  }
+  
+  
+  //SRS
+  
+  if (vrot){
+    if(mblock.hittest(0,0,vrot)){
+      console.log("SRS")
+      let rot0 = (mblock.rot+vrot)%4
+      let vx_s = vx,vy_s=vy
+      
+      let out = false
+      vx = 0
+      vy = 0
+      
+      for (let i=0;i<5;i++){
+        
+        switch(i){
+          case 0:
+          
+          if (mblock.type == 7){
+            if (rot0 == 1){
+              vx = 1
+            }else if (rot0 == 3){
+              vx = -1
+            }else{
+              vx = 2-mblock.rot
+              if (rot0 == 0)
+                vx *= 2
+            }
+          }else{
+            if (rot0 == 1){
+              vx = -1
+            }else if (rot0 == 3){
+              vx = 1
+            }else{
+              vx = 2-mblock.rot
+            }
+          }
+          
+          console.log(vx,mblock.rot)
+          break
+          
+          case 1:
+          
+          if (mblock.type == 7){
+            if (rot0 == 1){
+              vx = -2
+            }else if (rot0 == 3){
+              vx = 2
+            }else{
+              vx = mblock.rot-2
+              if (rot0 == 2)
+                vx *= 2
+            }
+          }else{
+            if (rot0 == 1 || rot0 == 3){
+              vy = -1
+            }else{
+              vy = 1
+            }
+          }
+          
+          break
+          
+          case 2:
+          
+          if (mblock.type == 7){
+            if (rot0 == 1){
+              vx = 1
+              vy = 1
+            }else if (rot0 == 3){
+              vx = -1
+              vy = -1
+            }else{
+              if (mblock.rot == 1){
+                vx = 2-mblock.rot
+                if (rot0 == 0)
+                  vx *= 2
+                vy = -1
+              }else{
+                vx = mblock.rot-2
+                if (rot0 == 2)
+                  vx *= 2
+                vy = 1
+              }
+            }
+            if (vrot == 3)
+              vy *= 2
+            
+          }else{
+            if (rot0 == 1 || rot0 == 3){
+              vx = 0
+              vy = 2
+            }else{
+              vx = 0
+              vy = -2
+            }
+          }
+          
+          break
+          
+          case 3:
+          
+          if (mblock.type == 7){
+            if (rot0 == 1){
+              vx = -2
+              vy = -1
+            }else if (rot0 == 3){
+              vx = 2
+              vy = 1
+            }else{
+              if (mblock.rot == 1){
+                vx = mblock.rot-2
+                if (rot0 == 2)
+                  vx *= 2
+                vy = 1
+              }else{
+                vx = 2-mblock.rot
+                if (rot0 == 0)
+                  vx *= 2
+                vy = -1
+              }
+            }
+            if (vrot == 1)
+              vy *= 2
+            
+          }else{
+            if (rot0 == 1){
+              vx = -1
+            }else if (rot0 == 3){
+              vx = 1
+            }else{
+              vx = 2-mblock.rot
+            }
+          }
+          
+          break
+          
+          case 4:
+          vx = vx_s
+          vy = vy_s
+          vrot = 0
+          
+          out = true
+          break
+        }
+        
+        
+        if (out)
+          break
+        
+        console.log(vx,vy,vrot,mblock.hittest(vx,vy,vrot))
+        if (!mblock.hittest(vx,vy,vrot))
+          break
+      }
+    }
+  }
+  
+  
+  if(mblock.hittest(0,vy,vrot)){
     vrot = 0
   }
   
+  
   if(mblock.hittest(vx,vy,vrot)){
-    let data = mblock.bldict()
+    let data = BlockFu.bldict(mblock.type,mblock.rot)
     for (let i=0;i<4;i++){
       let nx = data[i][0]+mblock.x
       let ny = data[i][1]+mblock.y
-      field.field[ny][nx] = mblock.type
+      if (field.field[ny])
+        field.field[ny][nx] = mblock.type
     }
     mblock = null
     field.del()
@@ -239,13 +661,21 @@ const main = () => {
     mblock.x += vx
     mblock.y += vy
     mblock.rot += vrot
+    mblock.rot %= 4
   }
   
-  
-  
-  
-  
   timer++
+  
+  }
+  
+  if(keydown[82]&&!keypushing[82]){
+    restart()
+    keypushing[82] = true
+  }
+  
+  if (keydown[32])
+    f_key = true
+  
   window.requestAnimationFrame(main)
 }
 
@@ -255,13 +685,25 @@ const main = () => {
 const restart = () => {
   
   timer = 0
-  ctxui.fillStyle = '#000'
-  ctxui.fillRect(0,0,480,480)
+  ctx.fillStyle = '#000'
+  ctx.fillRect(0,0,480,480)
+  
+  score = 0
+  lines = 0
+  level=1
+  ncase = []
+  nblock = []
+  mblock = 0
+  hold = [0,0]
+  f_end = false
+  
+  BGM.currentTime = 0.0
+  BGM.playbackRate = 1.0
+  BGM.play()
+  
   
   field = new Gamefield()
   field.disp()
-  
-  main()
 }
 
 // 初回処理
@@ -269,7 +711,7 @@ const init = () => {
   window.removeEventListener('click',init,false)
   keyInputSetting()
   restart()
-  //BGM.play()
+  main()
 }
 
 window.addEventListener('click',init,false)
